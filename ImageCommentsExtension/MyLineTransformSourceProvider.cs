750bb1b51c38afc18255ee1323880673318b99ae
Mark Lambert <marklam@spunow.co.uk>
2018-03-16 21:42:19 +0000

    Support F# language files

--- ImageCommentsExtension/MyLineTransformSourceProvider.cs
+++ ImageCommentsExtension/MyLineTransformSourceProvider.cs
@@ -6,7 +6,7 @@ using Microsoft.VisualStudio.Text.Formatting;
 namespace LM.ImageComments.EditorComponent
 {
     [Export(typeof(ILineTransformSourceProvider))]
-    [ContentType("CSharp"), ContentType("C/C++"), ContentType("Basic"), ContentType("Python")]
+    [ContentType("CSharp"), ContentType("C/C++"), ContentType("Basic"), ContentType("Python"), ContentType("F#")]
     [TextViewRole(PredefinedTextViewRoles.Document)]
     internal class MyLineTransformSourceProvider : ILineTransformSourceProvider
     {
