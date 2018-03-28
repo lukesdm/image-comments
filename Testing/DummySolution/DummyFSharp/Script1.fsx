a73d3b532ec5d4fb2c3cbf4925f5254a4a6704f9
Mark Lambert <marklam@spunow.co.uk>
2018-03-28 16:47:47 +0100

    Add F# test project

---
+++ Testing/DummySolution/DummyFSharp/Script1.fsx
@@ -0,0 +1,12 @@
+﻿/// <image url="$(ProjectDir)\CommentPics\Koala.jpg" scale="0.2" />
+/// Image author: David Iliff
+/// License: http://en.wikipedia.org/wiki/File:Koala_climbing_tree.jpg#Summary
+let Koala () =
+    ()
+
+/// <summary>
+/// <image url="$(SolutionDir)\CommonImages\Tile.jpg" />
+/// </summary>
+let Misc () =
+    ()
+
