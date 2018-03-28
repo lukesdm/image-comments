0f924c0b30b71173681a06340111f4316a9afcb5
Mark Lambert <marklam@spunow.co.uk>
2018-03-28 16:45:30 +0100

    Update extensibility projects to VS2017 format

--- ImageCommentsPackage/ImageCommentsPackage.cs
+++ ImageCommentsPackage/ImageCommentsPackage.cs
@@ -3,7 +3,8 @@
     using System;
     using System.ComponentModel.Design;
     using System.Diagnostics;
-    using System.Globalization;
+	using System.Diagnostics.CodeAnalysis;
+	using System.Globalization;
     using System.Runtime.InteropServices;
     using LM.ImageComments.EditorComponent;
     using Microsoft.VisualStudio.Shell;
@@ -15,6 +16,7 @@
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(GuidList.guidImageCommentsPackagePkgString)]
+	[SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
     public sealed class ImageCommentsPackage : Microsoft.VisualStudio.Shell.Package
     {
         #region Package Members
