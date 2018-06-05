1ca8796cb825137b038d913c1ff04fc2d72abb02
Mark Lambert <marklam@spunow.co.uk>
2018-06-05 22:43:15 +0100

    Had to make the package async to publish

---
+++ ImageCommentsPackage/ImageCommentsCommand.cs
@@ -0,0 +1,32 @@
+﻿namespace LM.ImageComments.Package
+{
+    using System.ComponentModel.Design;
+    using LM.ImageComments.EditorComponent;
+    using Microsoft.VisualStudio.Shell;
+    using Task = System.Threading.Tasks.Task;
+
+    internal sealed class ImageCommentsCommand
+    {
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            var mcs = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+
+            if (null != mcs)
+            {
+                var menuCommandID = new CommandID(GuidList.guidImageCommentsPackageCmdSet, (int)PkgCmdIDList.cmdidToggleImageComments);
+
+                var menuItem = new MenuCommand((s, e) => Execute(package), menuCommandID);
+
+                mcs.AddCommand(menuItem);
+            }
+        }
+
+        private static void Execute(AsyncPackage package)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            ImageAdornmentManager.ToggleEnabled();
+        }
+    }
+}
--- ImageCommentsPackage/ImageCommentsPackage.cs
+++ ImageCommentsPackage/ImageCommentsPackage.cs
@@ -1,46 +1,32 @@
 ﻿namespace LM.ImageComments.Package
 {
     using System;
-    using System.ComponentModel.Design;
     using System.Diagnostics;
-	using System.Diagnostics.CodeAnalysis;
-	using System.Globalization;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Globalization;
     using System.Runtime.InteropServices;
-    using LM.ImageComments.EditorComponent;
+    using System.Threading;
     using Microsoft.VisualStudio.Shell;
+    using Task = System.Threading.Tasks.Task;
 
     /// <summary>
     /// Package containing image comment command
     /// </summary>
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(GuidList.guidImageCommentsPackagePkgString)]
 	[SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
-    public sealed class ImageCommentsPackage : Microsoft.VisualStudio.Shell.Package
+    public sealed class ImageCommentsPackage : AsyncPackage
     {
-        #region Package Members
-
         /// <summary>
         /// Creates and registers image comment enable/disable toggle command 
         /// </summary>
-        protected override void Initialize()
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            ThreadHelper.ThrowIfNotOnUIThread();
-
-            Trace.WriteLine (string.Format(CultureInfo.CurrentCulture, "Entering Initialize() of: {0}", this.ToString()));
-            base.Initialize();
-
-            OleMenuCommandService mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
-            if ( null != mcs )
-            {
-                CommandID menuCommandID = new CommandID(GuidList.guidImageCommentsPackageCmdSet, (int)PkgCmdIDList.cmdidToggleImageComments);
-                MenuCommand menuItem = new MenuCommand(
-                    (sender, args) => { ImageAdornmentManager.ToggleEnabled(); }, 
-                    menuCommandID);
-                mcs.AddCommand( menuItem );
-            }
+            Trace.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering Initialize() of: {0}", this.ToString()));
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await ImageCommentsCommand.InitializeAsync(this);
         }
-        #endregion
     }
 }
