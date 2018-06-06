68cf705c9c8bf795e2226947308056e747e069fe
Thomas Pollak <tom.smart.bishop@gmail.com>
2018-06-06 15:47:26 +0100

    Reverting IDs from merge

--- ImageCommentsPackage/Guids.cs
+++ ImageCommentsPackage/Guids.cs
@@ -6,8 +6,8 @@ namespace LM.ImageComments.Package
 {
     static class GuidList
     {
-        public const string guidImageCommentsPackagePkgString = "B7EBBA5B-308D-4181-A115-7A8F92C1B85D";
-        public const string guidImageCommentsPackageCmdSetString = "EC327262-6A34-4ECF-B7A1-E02C4AC70765";
+        public const string guidImageCommentsPackagePkgString = "2980fb0d-ba9d-4373-9178-d27a98f0c9b1";
+        public const string guidImageCommentsPackageCmdSetString = "75607cbc-1a36-4075-8eb7-3ca58ae87f84";
 
         public static readonly Guid guidImageCommentsPackageCmdSet = new Guid(guidImageCommentsPackageCmdSetString);
     };
